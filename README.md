5abc35b5d1e1c52b6b62708f3bdc441362609030
thinhlamvi <thinhlamvi@gmail.com>
2016-09-17 02:07:07 +0700

    create admin project

---
+++ README.md
@@ -0,0 +1,4 @@
+thinh-project-symfony
+=====================
+
+A Symfony project created on September 17, 2016, 12:33 am.
---
+++ src/Cmn/UserBundle/CmnUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Cmn\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CmnUserBundle extends Bundle
+{
+}
